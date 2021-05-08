32c089cccf3d8e45a80b899cb89b7fa77c155748
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-05-08 13:19:12 -0400

    Update report.py

--- Solutions/3_2/report.py
+++ Solutions/3_2/report.py
@@ -1,6 +1,7 @@
 # report.py
 import csv
 
+
 def read_portfolio(filename):
     '''
     Read a stock portfolio file into a list of dictionaries with keys
@@ -14,13 +15,14 @@ def read_portfolio(filename):
         for row in rows:
             record = dict(zip(headers, row))
             stock = {
-                'name' : record['name'],
-                'shares' : int(record['shares']),
-                'price' : float(record['price'])
+                'name': record['name'],
+                'shares': int(record['shares']),
+                'price': float(record['price'])
             }
             portfolio.append(stock)
 
     return portfolio
+
 
 def read_prices(filename):
     '''
@@ -37,7 +39,8 @@ def read_prices(filename):
 
     return prices
 
-def make_report_data(portfolio,prices):
+
+def make_report_data(portfolio, prices):
     '''
     Make a list of (name, shares, price, change) tuples given a portfolio list
     and prices dictionary.
@@ -50,29 +53,32 @@ def make_report_data(portfolio,prices):
         rows.append(summary)
     return rows
 
+
 def print_report(reportdata):
     '''
     Print a nicely formated table from a list of (name, shares, price, change) tuples.
     '''
-    headers = ('Name','Shares','Price','Change')
+    headers = ('Name', 'Shares', 'Price', 'Change')
     print('%10s %10s %10s %10s' % headers)
     print(('-'*10 + ' ')*len(headers))
     for row in reportdata:
         print('%10s %10d %10.2f %10.2f' % row)
 
-def portfolio_report(portfoliofile,pricefile):        
+
+def portfolio_report(portfoliofile, pricefile):
     '''
     Make a stock report given portfolio and price data files.
     '''
-    # Read data files 
+    # Read data files
     portfolio = read_portfolio(portfoliofile)
     prices = read_prices(pricefile)
 
     # Create the report data
-    report = make_report_data(portfolio,prices)
+    report = make_report_data(portfolio, prices)
 
     # Print it out
     print_report(report)
+
 
 portfolio_report('../../Work/Data/portfolio.csv',
                  '../../Work/Data/prices.csv')
