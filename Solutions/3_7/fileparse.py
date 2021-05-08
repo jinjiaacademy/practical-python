497d2a439096711548397593ef29496926ba4104
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-05-08 13:19:15 -0400

    Update fileparse.py

--- Solutions/3_7/fileparse.py
+++ Solutions/3_7/fileparse.py
@@ -1,6 +1,7 @@
 # fileparse.py
 import csv
 
+
 def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
     '''
     Parse a CSV file into a list of records with type conversion.
@@ -11,9 +12,9 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
         # Read the file headers (if any)
         headers = next(rows) if has_headers else []
 
-        # If specific columns have been selected, make indices for filtering 
+        # If specific columns have been selected, make indices for filtering
         if select:
-            indices = [ headers.index(colname) for colname in select ]
+            indices = [headers.index(colname) for colname in select]
             headers = select
 
         records = []
@@ -23,7 +24,7 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
 
             # If specific column indices are selected, pick them out
             if select:
-                row = [ row[index] for index in indices]
+                row = [row[index] for index in indices]
 
             # Apply type conversion to the row
             if types:
