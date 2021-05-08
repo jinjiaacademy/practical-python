a393bb67127f358dbc5f11c050dbb764a7b13e57
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-05-08 13:18:23 -0400

    Update pcost.py

--- Solutions/8_2/pcost.py
+++ Solutions/8_2/pcost.py
@@ -2,6 +2,7 @@
 
 import report
 
+
 def portfolio_cost(filename):
     '''
     Computes the total cost (shares*price) of a portfolio file
@@ -9,11 +10,13 @@ def portfolio_cost(filename):
     portfolio = report.read_portfolio(filename)
     return portfolio.total_cost
 
+
 def main(args):
     if len(args) != 2:
         raise SystemExit('Usage: %s portfoliofile' % args[0])
     filename = args[1]
     print('Total cost:', portfolio_cost(filename))
+
 
 if __name__ == '__main__':
     import sys
