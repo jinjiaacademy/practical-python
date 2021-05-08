1ab8c7b15d98f13a0da9c893d10e103d31da06fc
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-05-08 13:18:49 -0400

    Update pcost.py

--- Solutions/3_14/pcost.py
+++ Solutions/3_14/pcost.py
@@ -1,6 +1,8 @@
 # pcost.py
 
+import sys
 import report
+
 
 def portfolio_cost(filename):
     '''
@@ -9,7 +11,7 @@ def portfolio_cost(filename):
     portfolio = report.read_portfolio(filename)
     return sum([s['shares']*s['price'] for s in portfolio])
 
-import sys
+
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
