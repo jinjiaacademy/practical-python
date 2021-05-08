fdf4a56c4ab2c2b16b5579321ba71bd2976400b3
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2021-05-08 13:18:08 -0400

    Update test_stock.py

--- Solutions/8_2/test_stock.py
+++ Solutions/8_2/test_stock.py
@@ -3,6 +3,7 @@
 import unittest
 import stock
 
+
 class TestStock(unittest.TestCase):
     def test_create(self):
         s = stock.Stock('GOOG', 100, 490.1)
@@ -23,6 +24,7 @@ class TestStock(unittest.TestCase):
         s = stock.Stock('GOOG', 100, 490.1)
         with self.assertRaises(TypeError):
             s.shares = '100'
+
 
 if __name__ == '__main__':
     unittest.main()
